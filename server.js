3b132c8edd94004ecb7e089255c9d1dac8976304
Dan Sexton <notxesnad@gmail.com>
2013-07-14 03:48:50 +0000

    swapped out code to heroku hello world example

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var http = require("http");
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
-http.createServer(function(request,response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen();
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
